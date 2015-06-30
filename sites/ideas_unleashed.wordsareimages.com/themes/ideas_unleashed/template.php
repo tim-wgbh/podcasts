e50624279ce2d59f64fd32a19e6333cde831e100
Tim Kinnel <tim_kinnel@wgbh.org>
2015-06-30 18:11:25 -0400

    Ideas Unleashed style updates

--- sites/ideas_unleashed.wordsareimages.com/themes/ideas_unleashed/template.php
+++ sites/ideas_unleashed.wordsareimages.com/themes/ideas_unleashed/template.php
@@ -1,3 +1,12 @@
 <?php
   drupal_add_js('/sites/caught.wordsareimages.com/themes/caught/caught.js');
+  drupal_add_css('sites/all/wgbh_links/wgbh_links.css');
+
+function ideas_unleashed_preprocess_page(&$vars) {
+  if (preg_match('/admin/', current_path()) || preg_match('/node\/add/', current_path())) {
+    $vars['admin_page'] = true;
+  } else {
+    $vars['admin_page'] = false;
+  }
+}
 ?>
